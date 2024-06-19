c91f00ed5113e557bbc5b424943f37767e5511f8
Herkules09 <mk301788@student.polsl.pl>
2024-06-19 17:08:07 +0200

    comit

---
+++ Backend/FitMeals/src/main/java/com/example/FitMeals/FitMealsApplication.java
@@ -0,0 +1,13 @@
+package com.example.FitMeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitMealsApplication.class, args);
+	}
+
+}
